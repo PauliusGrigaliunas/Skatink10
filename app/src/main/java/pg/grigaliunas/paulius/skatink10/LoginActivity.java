f98e8d960a94eee26e07a3e342e4c3514362b92e
Paulius <31667985+PauliusGrigaliunas@users.noreply.github.com>
2018-09-30 14:24:21 +0300

    Initial commit

---
+++ app/src/main/java/pg/grigaliunas/paulius/skatink10/LoginActivity.java
@@ -0,0 +1,27 @@
+package pg.grigaliunas.paulius.skatink10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn = (Button) findViewById(R.id.button);
+        OpenWindow();
+    }
+    public void OpenWindow() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
